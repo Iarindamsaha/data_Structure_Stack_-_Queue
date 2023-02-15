fe9a245eaf27749b5fc78f3056217863bc3e269e
Arindam Saha <asaha15071998@gmail.com>
2023-02-15 22:20:02 +0530

    Welcome Screen

---
+++ src/Stack_API/StackMain.java
@@ -0,0 +1,13 @@
+package Stack_API;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class StackMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("---Welcome To Stack And Queue Problems---");
+
+    }
+}
